0b27b3c295eee21bcc11b01488dc2ef1fcc5febb
darrenli03 <71399017+darrenli03@users.noreply.github.com>
2023-10-20 16:33:12 -0400

    switch to running from map (from Youtube API call)

--- src/Main.java
+++ src/Main.java
@@ -20,14 +20,14 @@ public class Main {
         GameRunnerFromTextFile gameRunnerFromTextFile = new GameRunnerFromTextFile();
         HashMap<String, String> videoMap = vs.getVideoMap();
 //      comment these lines out to switch between youtube api call and getting titles from text file
-//        ArrayList<Map.Entry<String, String>> entryArrayList = new ArrayList<>(videoMap.entrySet());
-        ArrayList<String> entryArrayList = new ArrayList<>(TextFileReader.readLinesFromFile(filePath));
+        ArrayList<Map.Entry<String, String>> entryArrayList = new ArrayList<>(videoMap.entrySet());
+//        ArrayList<String> entryArrayList = new ArrayList<>(TextFileReader.readLinesFromFile(filePath));
 
         System.out.println("number of entries: " + entryArrayList.size());
         Collections.shuffle(entryArrayList);
 //        comment these lines out to switch between running game from ArrayList of Map.Entry<String,String> or ArrayList<String>
-//        gameRunnerFromMap.runGameFromMap(entryArrayList);
-        gameRunnerFromTextFile.runGameFromTextFile(entryArrayList);
+        gameRunnerFromMap.runGameFromMap(entryArrayList);
+//        gameRunnerFromTextFile.runGameFromTextFile(entryArrayList);
 
     }
 }
