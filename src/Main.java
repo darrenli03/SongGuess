374a16e436fcbc87e38cab1e2efd2ea60e61a0e7
darrenli03 <71399017+darrenli03@users.noreply.github.com>
2023-10-20 15:02:25 -0400

    moved TextFileReader to separate class

--- src/Main.java
+++ src/Main.java
@@ -20,41 +20,14 @@ public class Main {
         GameRunnerFromTextFile gameRunnerFromTextFile = new GameRunnerFromTextFile();
         HashMap<String, String> videoMap = vs.getVideoMap();
 //      comment these lines out to switch between youtube api call and getting titles from text file
-        ArrayList<Map.Entry<String, String>> entryArrayList = new ArrayList<>(videoMap.entrySet());
-//        ArrayList<String> entryArrayList = new ArrayList<>(TextFileReader.readLinesFromFile(filePath));
+//        ArrayList<Map.Entry<String, String>> entryArrayList = new ArrayList<>(videoMap.entrySet());
+        ArrayList<String> entryArrayList = new ArrayList<>(TextFileReader.readLinesFromFile(filePath));
 
         System.out.println("number of entries: " + entryArrayList.size());
         Collections.shuffle(entryArrayList);
 //        comment these lines out to switch between running game from ArrayList of Map.Entry<String,String> or ArrayList<String>
-        gameRunnerFromMap.runGameFromMap(entryArrayList);
-//        gameRunnerFromTextFile.runGameFromTextFile(entryArrayList);
+//        gameRunnerFromMap.runGameFromMap(entryArrayList);
+        gameRunnerFromTextFile.runGameFromTextFile(entryArrayList);
 
     }
-
-
-    public static List<String> readLinesFromFile(String filePath) {
-        List<String> lines = new ArrayList<>();
-
-        try {
-            // Create a File object with the specified file path
-            File file = new File(filePath);
-
-            // Create a Scanner object to read from the file
-            Scanner scanner = new Scanner(file);
-
-            // Read lines from the file and add them to the list
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                lines.add(line);
-            }
-
-            // Close the scanner when done
-            scanner.close();
-        } catch (FileNotFoundException e) {
-            System.err.println("File not found: " + e.getMessage());
-        }
-
-        return lines;
-    }
-
 }
